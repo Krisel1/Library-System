d8e4da16b9ac22eebdf4d13db572f1cc775a2857
Ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-06-12 13:00:23 +0200

    Library

--- library/src/main/java/com/biblioteca/App.java
+++ library/src/main/java/com/biblioteca/App.java
@@ -7,8 +7,6 @@ public final class App {
     private App() {
     }
 
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ library/src/main/java/com/biblioteca/Library.java
@@ -0,0 +1,73 @@
+package com.biblioteca;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Library {
+    public static ArrayList<String> array = new ArrayList<>();
+
+    private static void createLibrary() {
+        try {
+            FileWriter paper = new FileWriter("Date_of_books", true);
+        } catch (Exception error) {
+            error.printStackTrace();
+        }
+    }
+
+    private static void fillLibrary() {
+        int arrayPosition = 0;
+        try {
+            PrintWriter writer = new PrintWriter("ToDoList.txt");
+            while (array.size() > arrayPosition) {
+                writer.println(array.get(arrayPosition));
+                arrayPosition++;
+            }
+            writer.close();
+        } catch(Exception error){
+            error.printStackTrace();
+        }
+    }
+
+    private static void fillArray() {
+        array.clear();
+        try {
+            FileReader read = new FileReader("ToDoList.txt");
+            Scanner arrayAdd = new Scanner(read);
+            while (arrayAdd.hasNextLine()) {
+                array.add(arrayAdd.nextLine());
+            }
+        } catch (FileNotFoundException Exception) {
+            Exception.printStackTrace();
+        }
+    }
+
+    private static void clearLibrary() {
+        try {
+            PrintWriter writer = new PrintWriter("ToDoList.txt");
+            writer.print("");
+            writer.close();
+        } catch (Exception error) {
+            error.printStackTrace();
+        }
+    }
+
+    public static void callCreateLibrary() {
+        createLibrary();
+    }
+
+    public static void callFillLibrary() {
+        fillLibrary();
+    }
+
+    public static void callFillArray() {
+        fillArray();
+    }
+
+    public static void callClearLibrary() {
+        clearLibrary();
+    }
+}
